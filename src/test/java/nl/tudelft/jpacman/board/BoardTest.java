f45f582e4a1bff1833fccaa21a5a5b8118176138
Pattaradnai Kaeodumkoeng <kafaircoffee2434well@gmail.com>
2024-11-05 14:57:28 +0700

    Update BoardTest.java

--- src/test/java/nl/tudelft/jpacman/board/BoardTest.java
+++ src/test/java/nl/tudelft/jpacman/board/BoardTest.java
@@ -1,19 +1,21 @@
 package nl.tudelft.jpacman.board;
-import static org.assertj.core.api.Assertions.assertThat;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
 
 public class BoardTest {
+
     @Test
-    public void createBoardTest(){
+    public void createBoardTest() {
         BasicSquare[][] bs = new BasicSquare[1][1];
         bs[0][0] = new BasicSquare();
 
         Board b = new Board(bs);
+
+        // Assert that the board and the square are valid
         assertThat(b.invariant()).isTrue();
-
+        assertThat(b.squareAt(0,0)).isNotNull();
         assertThat(b.squareAt(0,0).invariant()).isTrue();
-
     }
 
     @Test
@@ -21,9 +23,11 @@ public class BoardTest {
         BasicSquare[][] bs = new BasicSquare[1][1];
         bs[0][0] = null;
         Board b = new Board(bs);
+
+        // Assert that the board invariant is false due to null square
         assertThat(b.invariant()).isFalse();
-        assertThat(b.squareAt(0,0).invariant()).isTrue();
 
+        // Verify that squareAt(0, 0) returns null when accessed
+        assertThat(b.squareAt(0,0)).isNull();
     }
-
 }
